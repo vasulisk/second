88abd546c906baa9877c2f7ad1688137a13c311e
vasulisk <99254109+vasulisk@users.noreply.github.com>
2022-02-22 21:04:44 +0200

    Add files via upload

---
+++ second/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 69;
+        int b = a % 10;
+        int c = (a / 10) % 10;
+        System.out.println("Сума цифр числа a: " + (b + c));
+    }
+}
